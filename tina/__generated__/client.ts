d0a3db7048c2bb7c25538c84e4eb6bba22e717ac
Yours-truly-11611 <alexleon.email@gmail.com>
2026-03-03 22:30:25 -0500

    Switch to Decap CMS, remove all Tina code

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/alex/perilla/tina/__generated__/.cache/1772593392567', url: 'https://content.tinajs.io/2.2/content/620842f4-7117-4977-9729-bb457c5ac8aa/github/main', token: '49b78d57a28e26e4f5f67c503253cc32f7be9b49', queries,  });
-export default client;
-  
